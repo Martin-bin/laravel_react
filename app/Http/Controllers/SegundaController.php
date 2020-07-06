110e36dcf4b4f22d4670f099b778d63016a3e26c
Martin Diaz Gomez <mtnskate316@gmail.com>
2020-07-06 17:27:53 -0500

    Tabla empleados y segundas con comando php artisA

---
+++ app/Http/Controllers/SegundaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\segunda;
+use Illuminate\Http\Request;
+
+class SegundaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\segunda  $segunda
+     * @return \Illuminate\Http\Response
+     */
+    public function show(segunda $segunda)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\segunda  $segunda
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(segunda $segunda)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\segunda  $segunda
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, segunda $segunda)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\segunda  $segunda
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(segunda $segunda)
+    {
+        //
+    }
+}
--- database/migrations/2020_07_06_214435_create_segundas_table.php
+++ database/migrations/2020_07_06_214435_create_segundas_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateEmpleadosTable extends Migration
+class CreateSegundasTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,14 @@ class CreateEmpleadosTable extends Migration
      */
     public function up()
     {
-        Schema::create('empleados', function (Blueprint $table) {
+        Schema::create('segundas', function (Blueprint $table) {
             $table->bigIncrements('id');
 
-            $table->string('Nombre');
-            $table->string('Apellidos');
+            $table->string('nombre');
+            $table->string('apellido');
             $table->string('correo');
+            $table->string('edad');
+            $table->string('fecha');
 
             $table->timestamps();
         });
@@ -31,6 +33,6 @@ class CreateEmpleadosTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('empleados');
+        Schema::dropIfExists('segundas');
     }
 }
